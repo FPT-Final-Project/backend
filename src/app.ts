fd55845a9d5c7cb9b11a39f3c9f141b119e1c9c0
Long <hoanglongpham1522@gmail.com>
2021-06-24 23:45:56 +0700

    fix some bugs

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,8 @@
 import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
-
+import { PORT } from './configs';
 const corsOptions = { methods: ['GET', 'POST', 'PUT', 'DELETE'] };
 const app = express();
-const port = 3000;
 app.use(cors(corsOptions));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -12,6 +11,6 @@ app.get('/', (req: Request, res: Response, next: NextFunction) => {
   res.send('The sedulous hyena ate the antelope!');
 });
 
-app.listen(port, () => {
-  console.log(`server is listening on ${port}`);
+app.listen(PORT, () => {
+  console.log(`server is listening on ${PORT}`);
 });
