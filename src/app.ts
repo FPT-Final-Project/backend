05525d201240d6cdba7caadb92aca1891cbf278b
Long <hoanglongpham1522@gmail.com>
2021-06-24 23:49:09 +0700

    fix some bug

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,18 @@
 import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import { PORT } from './configs';
+import db from "./configs/mongoose"
+
 const corsOptions = { methods: ['GET', 'POST', 'PUT', 'DELETE'] };
 const app = express();
 app.use(cors(corsOptions));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+try {
+  db.connect();
+} catch (e) {
+  console.log("could not connect");
+}
 app.get('/', (req: Request, res: Response, next: NextFunction) => {
   res.send('The sedulous hyena ate the antelope!');
 });
