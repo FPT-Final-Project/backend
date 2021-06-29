d898b299c92322d6a568662bd5ff46f95bc9b646
Long <hoanglongpham1522@gmail.com>
2021-06-29 19:50:36 +0700

    fix conflicts again

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,5 @@
 import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
-<<<<<<< HEAD
 import helmet  from 'helmet';
 import morgan from 'morgan';
 import { PORT } from './configs';
@@ -29,20 +28,4 @@ app.get('/', (_req: Request, res: Response, _next: NextFunction) => {
 
 app.listen(PORT, () => {
   console.log(`server is listening on ${PORT}`);
-=======
-
-const corsOptions = { methods: ['GET', 'POST', 'PUT', 'DELETE'] };
-const app = express();
-const port = 3000;
-app.use(cors(corsOptions));
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-app.get('/', (req: Request, res: Response, next: NextFunction) => {
-  res.send('The sedulous hyena ate the antelope!');
-});
-
-app.listen(port, () => {
-  console.log(`server is listening on ${port}`);
->>>>>>> 5378b3b64cf3cdcba750edae6b81406478b337aa
 });
--- src/models/appointment.ts
+++ src/models/appointment.ts
@@ -1,17 +1,9 @@
 import * as mongoose from 'mongoose';
-<<<<<<< HEAD
 import { Document, Schema } from 'mongoose';
 /**
  * Appointment Interface
  */
 export interface IAppointment extends Document {
-=======
-
-/**
- * Appointment Interface
- */
-export interface Appointment {
->>>>>>> 5378b3b64cf3cdcba750edae6b81406478b337aa
   _id: string;
   dateOfAppointment: number;
   patentId: string;
@@ -23,11 +15,7 @@ export interface Appointment {
   isDeleted: boolean;
 }
 
-<<<<<<< HEAD
 export const AppointmentSchema: Schema = new Schema({
-=======
-export const AppointmentSchema = new mongoose.Schema({
->>>>>>> 5378b3b64cf3cdcba750edae6b81406478b337aa
   _id: { type: String, require: true },
   dateOfAppointment: { type: Number, require: true, index: true },
   patentId: { type: String, require: true, index: true },
@@ -36,11 +24,6 @@ export const AppointmentSchema = new mongoose.Schema({
   isCanceled: { type: Boolean, require: true, index: true },
   createdAt: { type: Number },
   updatedAt: { type: Number },
-<<<<<<< HEAD
   isDeleted: { type: Boolean },
 });
 export default mongoose.model<IAppointment>('Appointment', AppointmentSchema);
-=======
-  isDeleted: { type: Boolean }
-});
->>>>>>> 5378b3b64cf3cdcba750edae6b81406478b337aa
