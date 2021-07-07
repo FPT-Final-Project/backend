72a5291e947ac81ced658c52b247b666009c421e
Long <hoanglongpham1522@gmail.com>
2021-07-07 22:38:49 +0700

    fix some conflicts

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from 'express';
 import httpStatus from 'http-status';
 <<<<<<< HEAD
+<<<<<<< HEAD
 import catchAsync from '../utils/catchAsync';
 import { authService } from '../services';
 
@@ -8,6 +9,13 @@ import { authService } from '../services';
 import jwt from 'jsonwebtoken';
 import { TOKEN_SECRET } from '../configs';
 >>>>>>> 9fda1a9 ( fix yanr lint)
+=======
+import catchAsync from '../utils/catchAsync';
+import { authService } from '../services';
+
+import jwt from "jsonwebtoken";
+import {TOKEN_SECRET} from "../configs"
+>>>>>>> 24e6097 ( fix som bugs)
 const signup = catchAsync(
   async (req: Request, res: Response, _next: NextFunction) => {
     const {
@@ -29,11 +37,16 @@ const login = catchAsync(
     const { email, password } = req.body;
     const user = await authService.login(email, password);
 <<<<<<< HEAD
+<<<<<<< HEAD
     res.status(httpStatus.OK).json(user);
   },
 =======
     const accessToken = jwt.sign({id: user._id}, TOKEN_SECRET , { expiresIn: '1d' });
     res.status(httpStatus.OK).json({user, accessToken});
+=======
+    const token = jwt.sign({id: user._id}, TOKEN_SECRET , { expiresIn: '1d' });
+    res.status(httpStatus.OK).json({user,token});
+>>>>>>> 24e6097 ( fix som bugs)
   }
 >>>>>>> 9fda1a9 ( fix yanr lint)
 );
