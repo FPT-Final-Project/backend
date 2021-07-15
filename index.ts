4db91bd06e7dea66c067818cb062cda3a1f63a4f
bao ngo <ngobao3107@gmail.com>
2021-07-15 16:40:16 +0700

    feature callvideo

--- index.ts
+++ index.ts
@@ -1,12 +1,48 @@
 import express, { Express } from "express"
 import routes from "./src/routers"
-
-
+import { createServer } from "http";
+import { Server, Socket } from "socket.io";
+const { ExpressPeerServer } = require('peer');
 const app: Express = express()
+const port = 5000;
+const httpServer = createServer(app);
+const cors = require("cors");
 
-const PORT: string | number = process.env.PORT || 5000
+const peerServer = ExpressPeerServer(httpServer, {
+	debug: true,
+});
+
+const io = new Server(httpServer, {  
+	cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"]
+  }
+});
+
+let i  = 1;
+io.on("connection", (socket: Socket) => {
+	console.log('Socket da ket noi')
+	if(i < 3){
+		socket.on('join-room', (roomId, userId) => {
+			i = i + 1
+			socket.join(roomId)
+			socket.to(roomId).emit('user-connected', userId,i)
+			socket.on('sendMessage', (message,callback) => {
+				socket.to(roomId).emit('message', message, userId)
+				callback();
+			})
+			socket.on('disconnect', () => {
+				socket.to(roomId).emit('user-disconnected', userId)
+				i = i - 1
+			})
+		})
+	}
+})
 
 app.use(routes)
-app.listen(PORT, () => {
-    console.log(`server is listening on ${PORT}`);
-});
+app.use('/peerjs', peerServer)
+app.use(cors());
+
+httpServer.listen(port, () => {
+	console.log('Server listening on Port 5000');
+})
--- src/routers/index.ts
+++ src/routers/index.ts
@@ -4,11 +4,12 @@ const router: Router = Router()
 
 // router.get("/todos", getTodos)
 router.get('/', (req, res) => {
-    res.send('The sedulous hyenssssssssssa ate the antelope!');
+    res.send('Hello World!');
   });
 
-router.get('/info_videocall', (req, res) => { //Line 9
-  res.send({ username: 'Ngo Bao',partnername: 'Dr.The Duy' }); //Line 10
-}); //Line 11
+router.get('/info_videocall', (req, res) => { 
+  res.send({ username: 'Ngo Bao',partnername: 'Dr.The Duy' }); 
+});
+
 
 export default router
