1c1b33aa4371735481432ec557ca08ed4c54d1aa
bao ngo <ngobao3107@gmail.com>
2021-07-23 02:16:35 +0700

    backend chatvideo

--- index.ts
+++ index.ts
@@ -2,37 +2,30 @@ import express, { Express } from "express"
 import routes from "./src/routers"
 import { createServer } from "http";
 import { Server, Socket } from "socket.io";
-const { ExpressPeerServer } = require('peer');
 const app: Express = express()
 const port = 5000;
 const httpServer = createServer(app);
 const cors = require("cors");
 
-const peerServer = ExpressPeerServer(httpServer, {
-	debug: true,
-});
-
-const io = new Server(httpServer, {  
-	cors: {
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST"]
-  }
-});
-
+const io = new Server(httpServer, {});
 let i  = 1;
+let listUser: any[] = []
 io.on("connection", (socket: Socket) => {
-	console.log('Socket da ket noi')
 	if(i < 3){
-		socket.on('join-room', (roomId, userId) => {
+		socket.on('join-room', (roomId, userId, peerId) => {
+			console.log(`${userId} da join vao room ${roomId}`)
 			i = i + 1
 			socket.join(roomId)
-			socket.to(roomId).emit('user-connected', userId,i)
+			socket.to(roomId).emit('user-connected', userId, peerId)
+			listUser.push(userId);
+			io.to(roomId).emit('getUsersInRoom',listUser);
 			socket.on('sendMessage', (message,callback) => {
-				socket.to(roomId).emit('message', message, userId)
+				io.to(roomId).emit('message', { text: message, userid: userId })
 				callback();
 			})
-			socket.on('disconnect', () => {
+			socket.on('disconnect', (userid) => {
 				socket.to(roomId).emit('user-disconnected', userId)
+				listUser.splice(listUser.indexOf(userid), 1);
 				i = i - 1
 			})
 		})
@@ -40,7 +33,6 @@ io.on("connection", (socket: Socket) => {
 })
 
 app.use(routes)
-app.use('/peerjs', peerServer)
 app.use(cors());
 
 httpServer.listen(port, () => {
