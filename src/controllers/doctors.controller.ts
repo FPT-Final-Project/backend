4a2579bf2aeac35618b75bad39a001013ad78901
vietnp14 <vietnp14@gmail.com>
2021-08-01 23:03:40 +0700

    refactor request interface

--- src/controllers/doctors.controller.ts
+++ src/controllers/doctors.controller.ts
@@ -2,9 +2,10 @@ import { NextFunction, Request, Response } from 'express';
 import httpStatus from 'http-status';
 import catchAsync from '../utils/catchAsync';
 import { userService } from '../services';
+import { IRequest } from '../configs/request';
 
 const getDoctors = catchAsync(
-  async (req: Request, res: Response, _: NextFunction) => {
+  async (req: IRequest, res: Response, _: NextFunction) => {
   },
 );
 const getListOfDoctors = catchAsync(async (req: Request, res: Response, _:NextFunction) => {
